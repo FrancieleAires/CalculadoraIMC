8a9065256ae59220a8d60ab4345b9b96b01df5fb
FrancieleAires <f.aires2016@gmail.com>
2023-12-13 21:32:25 -0300

    commit

---
+++ CalculadoraIMC/CalcularIMC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    internal class CalcularIMC
+    {
+
+        public static double CalculandoIMC(double peso, double altura)
+        {
+            
+            return peso / (altura * altura);
+        }
+        public void InterpretandoIMC(double imc)
+        {
+            if (imc < 18.4)
+            {
+                Console.WriteLine("Abaixo do peso!");
+            }
+            else if (imc >= 18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("Normal!");
+            }
+            else if (imc >= 25 && imc <=29.9)
+            {
+                Console.WriteLine("Sobrepeso!");
+            }
+            else if (imc >= 30 &&  imc <= 34.9)
+            {
+                Console.WriteLine("Obesidade I!");
+            }
+            else if (imc >= 35 && imc <= 39.9)
+            {
+                Console.WriteLine("Obesidade II!");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade III");
+            }
+        }
+    }
+}
---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CalculadoraIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabela valores do IMC - 20 a 60 anos.");
+            Console.WriteLine("╔════════════════════════════╗");
+            Console.WriteLine("║Abaixo do peso |  < 18.4    ║");
+            Console.WriteLine("║Normal         | 18.5 - 24.9║");
+            Console.WriteLine("║Sobrepeso      | 25 - 29.9  ║");
+            Console.WriteLine("║Obesidade I    | 30 - 34.9  ║");
+            Console.WriteLine("║Obesidade II   | 35 - 39.9  ║");
+            Console.WriteLine("║Obesidade III  |  >  40     ║");
+            Console.WriteLine("╚════════════════════════════╝");
+
+            Console.WriteLine("Digite seu peso: ");
+            double peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite sua altura: ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CalcularIMC calcular = new CalcularIMC();
+            Console.WriteLine();
+            double imc = CalcularIMC.CalculandoIMC(peso, altura);
+         
+
+            Console.WriteLine("Seu IMC é: " + imc.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Seu IMC na tabela é: " );
+            calcular.InterpretandoIMC(imc);
+            
+            
+        }
+    }
+}
